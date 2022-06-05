81ac22f91b1606618d95208e06f1b2a0b7a799f9
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-05 15:54:50 -0700

    up-to-date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_192448) do
+ActiveRecord::Schema.define(version: 2022_06_05_225242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_06_05_192448) do
     t.string "email"
     t.string "password_hash"
     t.string "password_salt"
+    t.boolean "admin", default: false
   end
 
 end
